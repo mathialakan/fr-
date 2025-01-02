98be51ffc4a7a032f144996b08aa2b43f94e8743
Mathialakan Thavappiragasam <mthavappiragasam@Mathialakans-MacBook-Pro.local>
2025-01-02 08:43:11 -0600

    include omp and acc acceleration

--- mmm-test/mmm.cpp
+++ mmm-test/mmm.cpp
@@ -5,6 +5,12 @@
 #include <chrono>
 #include "../fraction/fr.h"
 
+
+#if defined(OMP)
+#include <omp>
+#elif defined(OMP_OL)
+#define team_size 256
+#endif
 using namespace std;
 
 
@@ -54,6 +60,21 @@ template<typename et>
 et* mat_mul(et* A, et* B, int N, int K, int M){
 
     et* C = (et*)malloc(N*M *sizeof(et));
+    #if defined(OMP_OL)
+        #pragma omp target enter data map(alloc: C[:n*m])
+    #elif defined(OACC)
+        #pragma acc enter data create(C[:n*m])
+    #endif
+
+    #if defined(OMP)
+        #pragma omp parallel for collapse(2) schedule(dynamic)
+    #elif defined(OMP_OL)
+        #pragma omp target teams distributed parallel for collapse(2) \
+            thread_limit(team_size) num_teams((N*M +1)/team_size) 
+    #elif defined(OACC)
+        #pragma acc parallel loop present(A, B, C) gang worker \
+                num_workers(team_size) vector_length(32)
+    #endif
     for(int i=0; i<N; i++)
         for(int j=0; j<M; j++){
             C[M*i +j]= 0;
@@ -99,6 +120,20 @@ et** mat_maul(et** A, et** B){
     for(int i=0; i<N; i++)
         C[i] = (et*)malloc(M*sizeof(et));
 
+    #if defined(OMP_OL)
+        #pragma omp target enter data map(alloc: C[:n])
+    #elif defined(OACC)
+        #pragma acc enter data create(C[:n])
+    #endif
+
+    #if defined(OMP)
+        #pragma omp parallel for collapse(2) schedule(dynamic) 
+    #elif defined(OMP_OL)
+        #pragma omp target teams distribute parallel for \
+                thread_limit(team_size) num_teams((N*M +1)/team_size)
+    #elif defined(OACC)
+        #pragma acc parallel loop
+    #endif
     for(int i=0; i<N; i++)
         for(int j=0; j<M; j++){
             C[i][j] = 0;
@@ -115,6 +150,20 @@ et* mat_mul_cpp(et* A, et* B, int N, int K, int M){
 
     et* C = new et[N*M];
     
+    #if defined(OMP_OL)
+        #pragma omp target enter data map(alloc: C[:n*m])
+    #elif defined(OACC)
+        #pragma acc enter data create(C[:n*m])
+    #endif
+
+    #if defined(OMP)
+        #pragma omp parallel for collapse(2) schedule(dynamic)
+    #elif defined(OMP_OL)
+        #pragma omp taraget teams distribute parallel for \
+                thread_limit(team_size) num_teams((N*M +1)/team_size)
+    #elif defined(OACC)
+        #pragma acc parallel loop
+    #endif
     for(int i=0; i<N; i++)
     for(int j=0; j<M; j++){
         C[M*(i-1) +j] = 0;
@@ -137,6 +186,20 @@ et** mat_maul_cpp(et** A, et** B){
     for (int i=0; i<N; i++)
         C[i] = new et[M];
 
+    #if defined(OMP_OL)
+        #pragma omp target enter data map(alloc: C[:n])
+    #elif defined(OACC)
+        #pragma acc enter data create(C[:n])
+    #endif
+
+    #if defined(OMP)
+        #pragma omp parallel for collapse(2) schedule(dynamic)
+    #elif defined(OMP_OL)
+        #pragma omp taraget teams distribute parallel for \
+                thread_limit(team_size) num_teams((N*M +1)/team_size)
+    #elif defined(OACC)
+        #pragma acc parallel loop
+    #endif
     for (int i=0; i<N; i++)
         for( int j=0; j<M; j++){
             C[i][j]=0;
@@ -166,6 +229,14 @@ vector<vector<et> > mat_mul(vector<vector<et> > A, vector<vector<et> > B){
     //if (K != B.size()) return 0;
     vector<vector<et> > C(N, vector<et>(M));
 
+    #if defined(OMP)
+        #pragma omp parallel for collapse(2) schedule(dynamic)
+    #elif defined(OMP_OL)
+        #pragma omp taraget teams distribute parallel for \
+                thread_limit(team_size) num_teams((N*M +1)/team_size)
+    #elif defined(OACC)
+        #pragma acc parallel loop
+    #endif  
     for(int i=0; i< N; ++i)
         for(int j=0; j< M; ++j){
            C[i][j] = 0;
@@ -231,6 +302,11 @@ unordered_map<string, double> test_mm(int n, int k, int m){
 
     fill_mat(A, n, k);
     fill_mat(B, k, m);
+    #if defined(OMP_OL)
+        #prgama omp target enter data map(to:A[0:n*k],B[0:k*m])
+    #elif defined(OACC)
+        #pragma acc enter data copyin(A[:n*k],B[:k*m])
+    #endif
     auto end_time = chrono::steady_clock::now();
     time_local["fill_mat"] = chrono::duration_cast<chrono::microseconds>(end_time -start_time).count();
     start_time = end_time;
@@ -248,10 +324,21 @@ unordered_map<string, double> test_mm(int n, int k, int m){
     start_time = end_time;
 
     cout << "Result" << endl;
+    #if defined(OMP_OL)
+        #prgama omp target update from(C[0:n*m])
+    #elif defined(OACC)
+        #pragma acc update self(C[0:n*m])
+    #endif
     print_mat<et>(C, n, m);
     end_time = chrono::steady_clock::now();
     time_local["print_post-mat"] = chrono::duration_cast<chrono::microseconds>(end_time -start_time).count();
     //start_time = end_time;
+
+    #if defined(OMP_OL)
+        #prgama omp target exit data map(delete:A[0:n*k],B[0:k*m],C[0:m*n])
+    #elif defined(OACC)
+        #pragma acc exit data delete(A[:n*k],B[:k*m],C[0:m*n])
+    #endif
 
     return time_local;
 }
@@ -334,13 +421,37 @@ void efficiency_analysis(unordered_map<string, double> time_map ){
     cout<< "----------------------------------" << endl;
     for(auto& pair : time_map)
         cout<< pair.first << "\t" << pair.second << endl;
+    cout<< "----------------------------------" << endl <<endl;
+
+}
+
+void efficiency_analysis(unordered_map<string, double> time_map , size_t dsize, size_t tsize){
+
+    cout<< "Data size (in Bytes) : " <<dsize<<"\t Type size (in Bytes) : " << tsize  << endl;
+    cout<< "----------------------------------" << endl;
+    cout<< "Task" << "\t" << "Time (micro-sec)" << endl;
+    cout<< "----------------------------------" << endl;
+    for(auto& pair : time_map)
+        cout<< pair.first << "\t" << pair.second << endl;
+    cout<< "----------------------------------" << endl <<endl;
+
+    cout<< "----------------------------------" << endl;
+    cout<< "Task" << "\t" << "Bandwidth (GB/s)" << endl;
+    cout<< "----------------------------------" << endl;
+    //auto seconds = 0;
+    //auto gigabytes = 0;
+    for(auto& pair : time_map){
+        auto seconds = pair.second * 1.e-6;
+        auto gigabytes = (double)(dsize*tsize)* 1.e-9; // GB
+        cout<< pair.first << "\t" << (gigabytes/seconds) << endl;
+    }       
     cout<< "----------------------------------" << endl;
 }
 
 int main(){
     
-    //int n = 1000, m = 1000, k=1000;
-    int n = 2, m = 2, k=2;
+    int n = 100, m = 100, k=100;
+    //int n = 2, m = 2, k=2;
     // 2D-Vector of floats
     /*
     vector<vector<float> > A(n, vector<float>(k));
@@ -358,33 +469,35 @@ int main(){
 
     unordered_map<string, double> time_map;
     auto start_time = chrono::steady_clock::now();
-    auto time_int = test_mm<int>(12, 12, 12); cout<< "Finished MMM using 1-D array of integers" << endl; //Test 1-D array
+    auto time_int = test_mm<int>(n, k, m); cout<< "Finished MMM using 1-D array of integers" << endl; //Test 1-D array
     auto end_time = chrono::steady_clock::now();
     time_map["1d-int"] = chrono::duration_cast<chrono::microseconds>(end_time -start_time).count();
     start_time = end_time;
-    auto time_fl = test_mm<float>(12, 12, 12); cout<< "Finished MMM using 1-D array of floats" << endl; //Test 1-D array
+    auto time_fl = test_mm<float>(n, k, m); cout<< "Finished MMM using 1-D array of floats" << endl; //Test 1-D array
     end_time = chrono::steady_clock::now();
     time_map["1d-float"] = chrono::duration_cast<chrono::microseconds>(end_time -start_time).count();
     start_time = end_time;
-    auto time_fr = test_mm_fr<fraction>(12, 12, 12); cout<< "Finished MMM using 1-D array of fractions" << endl; //Test Fraction
+    auto time_fr = test_mm_fr<fraction>(n, k, m); cout<< "Finished MMM using 1-D array of fractions" << endl; //Test Fraction
     end_time = chrono::steady_clock::now();
     time_map["1d-fraction"] = chrono::duration_cast<chrono::microseconds>(end_time -start_time).count();
     start_time = end_time;
-    auto time_mfr = test_mm_mfr<mixed>(12, 12, 12); cout<< "Finished MMM using 1-D array of mixed fractions" << endl; //Test Fraction
+    auto time_mfr = test_mm_mfr<mixed>(n, k, m); cout<< "Finished MMM using 1-D array of mixed fractions" << endl; //Test Fraction
     end_time = chrono::steady_clock::now();
     time_map["1d-mixed"] = chrono::duration_cast<chrono::microseconds>(end_time -start_time).count();
     start_time = end_time;
 
+    size_t dsize = n*k +k*m +2*n*m;
     cout<< endl << "Total Time " << endl;
     efficiency_analysis(time_map);
     cout<< endl << "1D-int Time " << endl;
-    efficiency_analysis(time_int);
+    efficiency_analysis(time_int, dsize, sizeof(int)); 
     cout<< endl << "1D-float Time " << endl;
-    efficiency_analysis(time_fl);
+    efficiency_analysis(time_fl, dsize, sizeof(float));
     cout<< endl << "1D-fraction Time " << endl;
-    efficiency_analysis(time_fr);
+    efficiency_analysis(time_fr, dsize, sizeof(fraction));
     cout<< endl << "1D-mixed Time " << endl;
-    efficiency_analysis(time_mfr);
+    efficiency_analysis(time_mfr, dsize, sizeof(mixed));
 
+    
     return 0;
 }
